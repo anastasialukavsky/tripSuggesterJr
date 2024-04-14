8a74d9370e3650a81ede513022b7e4ae507c902f
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-14 17:08:00 -0400

    add test MessageController class

--- src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/TripSuggesterJrApplication.kt
+++ src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/TripSuggesterJrApplication.kt
@@ -2,10 +2,19 @@ package dev.tripsuggesterjr.tripSuggesterJr
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class TripSuggesterJrApplication
 
 fun main(args: Array<String>) {
 	runApplication<TripSuggesterJrApplication>(*args)
+}
+
+@RestController
+class MessageController {
+	@GetMapping("/")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
