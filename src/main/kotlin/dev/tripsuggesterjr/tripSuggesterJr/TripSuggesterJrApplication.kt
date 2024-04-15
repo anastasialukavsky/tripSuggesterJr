9fc7cf5b7ff30fc0f5f62b2b69e7c691eb35c5ac
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-15 17:03:14 -0400

    remove test rest controller for message

--- src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/TripSuggesterJrApplication.kt
+++ src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/TripSuggesterJrApplication.kt
@@ -2,9 +2,7 @@ package dev.tripsuggesterjr.tripSuggesterJr
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
 class TripSuggesterJrApplication
@@ -13,14 +11,14 @@ fun main(args: Array<String>) {
 	runApplication<TripSuggesterJrApplication>(*args)
 }
 
-@RestController
-class MessageController {
-	@GetMapping("/")
-	fun index() = listOf(
-		Message("1", "Hello!"),
-		Message("2", "Bonjour!"),
-		Message("3", "Privet!"),
-	)
-}
+//@RestController
+//class MessageController {
+//	@GetMapping("/")
+//	fun index() = listOf(
+//		Message("1", "Hello!"),
+//		Message("2", "Bonjour!"),
+//		Message("3", "Privet!"),
+//	)
+//}
 
-data class Message(val id: String?, val text: String)
+//data class Message(val id: String?, val text: String)
