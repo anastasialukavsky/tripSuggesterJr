4f440ede76e648fa8da8bdc3d878be11188926ae
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-16 13:14:17 -0400

    add try catch for put and delete methods; throw custom exceptions

--- src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/services/TodoService.kt
+++ src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/services/TodoService.kt
@@ -1,8 +1,11 @@
 package dev.tripsuggesterjr.tripSuggesterJr.services
 
+import dev.tripsuggesterjr.tripSuggesterJr.exceptions.DataNotFoundException
 import dev.tripsuggesterjr.tripSuggesterJr.models.Todo
 import dev.tripsuggesterjr.tripSuggesterJr.repositories.TodoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 
@@ -17,21 +20,36 @@ class TodoService {
     }
 
     fun updateTodo(todoEntity: Todo, todoId: Long): Todo {
-        val newTodo: Todo = todoRepo.getReferenceById(todoId)
-        newTodo.title = todoEntity.title
-        newTodo.description = todoEntity.description
-        newTodo.progress = todoEntity.progress
+        if (todoEntity.id == todoId) {
+            return try {
+                val newTodo: Todo = todoRepo.getReferenceById(todoId)
+                newTodo.title = todoEntity.title
+                newTodo.description = todoEntity.description
+                newTodo.progress = todoEntity.progress
+                todoRepo.save(newTodo)
+            } catch (e: Exception) {
+                throw Exception("Unable to update data with id $todoId")
+            }
 
+        }
 
-        return todoRepo.save(newTodo)
+        throw DataNotFoundException("Id used not valid")
     }
 
     fun retrieveAllTodos(): List<Todo>? {
         return todoRepo.findAll()
     }
 
-    fun deleteTodoById(todoId: Long): String {
-        todoRepo.deleteById(todoId)
-        return "deleted"
+    fun deleteTodoById(todoId: Long): ResponseEntity<Map<String, String>> {
+        return try {
+            todoRepo.deleteById(todoId)
+            val dataMessage: MutableMap<String, String> = HashMap()
+            dataMessage["data"] = "Property removed"
+            ResponseEntity.ok(dataMessage)
+        } catch (e: EmptyResultDataAccessException) {
+            throw DataNotFoundException("Unable to delete item, id not found")
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
