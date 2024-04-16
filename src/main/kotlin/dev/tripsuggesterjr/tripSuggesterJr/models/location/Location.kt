da8339c4144bcdfa779706beebf0b4d32f7aa712
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-16 13:19:17 -0400

    add base location schema

---
+++ src/main/kotlin/dev/tripsuggesterjr/tripSuggesterJr/models/location/Location.kt
@@ -0,0 +1,19 @@
+package dev.tripsuggesterjr.tripSuggesterJr.models.location
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import java.util.*
+
+@Entity
+data class Location(
+    @Id
+    @GeneratedValue()
+    var locationId: UUID = UUID.randomUUID(),
+
+    var latitude: Double = 0.0,
+
+    var longitude: Double = 0.0
+
+
+)
